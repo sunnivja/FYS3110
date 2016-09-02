e201455f78c17f354cde81022f8278f6c4fa2824
sunnivja <sunnivja@student.matnat.uio.no>
2016-09-02 14:21:22 +0200

    Iterasjoner
    Skrevet for-løkker for forwards- og backwards substitusjon. Prøvd å få
    skrevet en fil, men det er en rar feil et sted.

--- qttest/test/main.cpp
+++ qttest/test/main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    int number = 5;
+    int* address = &number; // Pointer
+    cout << "Number:" << (long)number << endl;
+    cout << "Address:" << (long)address << endl;
+
+    *address = 10.4;
+    cout << "number:" << (long)number << endl;
+    cout << "number:" << (long)address << endl;
     return 0;
 }
